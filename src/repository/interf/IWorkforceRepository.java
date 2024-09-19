57d74a2c776625770d25985ef3459188609ad1e9
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-09-19 11:05:20 +0200

    Refactor WorkforceRepository to include CRUD operations for Workforce model

--- src/repository/interf/IWorkforceRepository.java
+++ src/repository/interf/IWorkforceRepository.java
@@ -1,4 +1,15 @@
 package repository.interf;
 
+import model.Workforce;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IWorkforceRepository {
+    boolean addWorkforce(Workforce workforce);
+    boolean updateWorkforce(Workforce workforce);
+    boolean deleteWorkforce(int id);
+    Optional<Workforce> getWorkforce(int id);
+    Optional<List<Workforce>> getWorkforces();
+    Optional<List<Workforce>> getWorkforcesByProject(int projectId);
 }
