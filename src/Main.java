80df60b3beb85fc5f9ee567e429a5fd82e684fdc
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-09-19 11:40:47 +0200

    - Add imports for Client, Material, and MaterialRepository - Modify main method to call CientsMenu() method - Modify getAllClients() method in ClientController to be public

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import controller.ClientController;
+import model.Client;
+import model.Material;
+import repository.MaterialRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
         ClientController clientController = new ClientController();
-    }
-}
+        clientController.CientsMenu();
+
+
+
+}}
