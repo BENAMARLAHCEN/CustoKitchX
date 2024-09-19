3232458681a7cfd585c75b2c8e5b156e17afdefe
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-09-19 11:04:37 +0200

    Refactor Material class to include additional properties and ResultSet conversion method

--- src/model/Material.java
+++ src/model/Material.java
@@ -1,6 +1,13 @@
 package model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Material extends Component {
+    private double coutUnitaire;
+    private double quantite;
     private double coutTransport;
     private double coefficientQualite;
 
@@ -8,13 +15,17 @@ public class Material extends Component {
     }
 
     public Material(String nom, double coutUnitaire, double quantite, double coutTransport, double coefficientQualite, int projetId) {
-        super(nom, coutUnitaire, quantite, "MATERIEL", projetId);
+        super(nom, "Matériel", projetId);
+        this.coutUnitaire = coutUnitaire;
+        this.quantite = quantite;
         this.coutTransport = coutTransport;
         this.coefficientQualite = coefficientQualite;
     }
 
     public Material(int id, String nom, double coutUnitaire, double quantite, String typeComposant, double tauxTVA, int projetId, double coutTransport, double coefficientQualite) {
-        super(id, nom, coutUnitaire, quantite, typeComposant, tauxTVA, projetId);
+        super(id, nom, typeComposant, tauxTVA, projetId);
+        this.coutUnitaire = coutUnitaire;
+        this.quantite = quantite;
         this.coutTransport = coutTransport;
         this.coefficientQualite = coefficientQualite;
     }
@@ -35,5 +46,44 @@ public class Material extends Component {
 
     public void setCoefficientQualite(double coefficientQualite) {
         this.coefficientQualite = coefficientQualite;
+    }
+
+    public double getCoutUnitaire() {
+        return coutUnitaire;
+    }
+
+    public void setCoutUnitaire(double coutUnitaire) {
+        this.coutUnitaire = coutUnitaire;
+    }
+
+    public double getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(double quantite) {
+        this.quantite = quantite;
+    }
+
+    public static List<Material> fromResultSet(ResultSet resultSet) throws SQLException {
+        List<Material> materials = new ArrayList<>();
+        try {
+            while (resultSet.next()) {
+                Material material = new Material();
+                material.setId(resultSet.getInt("id"));
+                material.setNom(resultSet.getString("nom"));
+                material.setTypeComposant(resultSet.getString("type_composant"));
+                material.setTauxTVA(resultSet.getDouble("taux_tva"));
+                material.setProjetId(resultSet.getInt("projet_id"));
+                material.setCoutUnitaire(resultSet.getDouble("cout_unitaire"));
+                material.setQuantite(resultSet.getDouble("quantite"));
+                material.setCoutTransport(resultSet.getDouble("cout_transport"));
+                material.setCoefficientQualite(resultSet.getDouble("coefficient_qualite"));
+                materials.add(material);
+            }
+            return materials;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
